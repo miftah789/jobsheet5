afc1efbf2a4e6424e532a24f15be8608c7084a9a
miftahul jannah <miftah074@gmail.com>
2018-01-09 18:58:34 +0700

    AddIdlingResourceMenuActivityTest

---
+++ app/src/androidTest/java/com/example/android/teatime/IdlingResourceMenuActivityTest.java
@@ -0,0 +1,82 @@
+/*
+* Copyright (C) 2017 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*  	http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.example.android.teatime;
+
+
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.IdlingResource;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.anything;
+
+/**
+ * This is an example instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+
+
+@RunWith(AndroidJUnit4.class)
+public class IdlingResourceMenuActivityTest {
+
+    /**
+     * The ActivityTestRule is a rule provided by Android used for functional testing of a single
+     * activity. The activity that will be tested, MenuActivity in this case, will be launched
+     * before each test that's annotated with @Test and before methods annotated with @Before.
+     * <p>
+     * The activity will be terminated after the test and methods annotated with @After are
+     * complete. This rule allows you to directly access the activity during the test.
+     */
+    @Rule
+    public ActivityTestRule<MenuActivity> mActivityTestRule =
+            new ActivityTestRule<>(MenuActivity.class);
+
+    private IdlingResource mIdlingResource;
+
+
+    // Registers any resource that needs to be synchronized with Espresso before the test is run.
+    @Before
+    public void registerIdlingResource() {
+        mIdlingResource = mActivityTestRule.getActivity().getIdlingResource();
+        // To prove that the test fails, omit this call:
+        Espresso.registerIdlingResources(mIdlingResource);
+    }
+
+    @Test
+    public void idlingResourceTest() {
+        onData(anything()).inAdapterView(withId(R.id.tea_grid_view)).atPosition(0).perform(click());
+    }
+
+    // Remember to unregister resources when not needed to avoid malfunction.
+    @After
+    public void unregisterIdlingResource() {
+        if (mIdlingResource != null) {
+            Espresso.unregisterIdlingResources(mIdlingResource);
+        }
+    }
+
+}
